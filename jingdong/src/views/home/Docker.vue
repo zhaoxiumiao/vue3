96c67178462052f7f1caf57fd52004f506b43ec0
zhao xiu miao <1027583934@qq.com>
2021-04-27 01:20:08 +0800

    "login"

---
+++ jingdong/src/views/home/Docker.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="docker">
+    <section 
+        v-for="(item, index) in dockerList" 
+        :class="{'docker__item':true,'docker__item--active': index===0}"
+        :key="item.icon"
+    >
+      <div class="iconfont" v-html="item.icon"></div>
+      <div class="docker__title">{{item.text}}</div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Docker',
+    setup(){
+        const dockerList = [
+            {icon:'&#xe6f9;', text: '首页'},
+            {icon:'&#xe6bc;', text: '购物车'},
+            {icon:'&#xe61e;', text: '顶单'},
+            {icon:'&#xe78b;', text: '我的'}
+        ]
+        return{ dockerList }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../style/viriables.scss';
+.docker{
+  display: flex;
+  box-sizing: border-box;
+  position: absolute;
+  padding: 0 .18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: .49rem;
+  border-top: .01rem solid $content-bgColor;
+  color: $content-fontcolor;
+  &__item{
+    flex: 1;
+    text-align: center;
+    .iconfont{
+      margin: .07rem 0 .02rem 0;
+      font-size: .18rem;
+    }
+    &--active{
+      color: #1FA4FC;
+    }
+  }
+  &__title{
+    font-size: .2rem;
+    transform: scale(.5, .5);
+    transform-origin: center top;
+  }
+}
+</style>
---
+++ jingdong/src/views/home/StaticPart.vue
@@ -0,0 +1,160 @@
+<template>
+<div>
+  <div class="position">
+    <span class="iconfont position__icon">&#xe64f;</span>
+    北京理工大学国防科技园
+    <span class="iconfont position__notice">&#xe860;</span>
+  </div>
+  <div class="search">
+    <span class="iconfont">&#xe64c;</span>
+    <span class="search__text">山姆会员</span>
+  </div>
+  <div class="banner">
+    <img
+      class="banner__img"
+      src="http://www.dell-lee.com/imgs/vue3/banner.jpg"
+      alt=""
+    />
+  </div>
+  <div class="icons">
+    <div 
+      class="icons__item"
+      v-for="(item,index) in iconsList"
+      :key="index"
+    >
+      <img
+        :src="item.imgUrl"
+        class="icons__item__img"
+      />
+      <p class="icons__item__desc">{{item.title}}</p>
+    </div>
+    
+  </div>
+  <div class="gap"></div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "StaticPart",
+  setup(){
+    const iconsList=[
+      {
+        imgUrl:'http://www.dell-lee.com/imgs/vue3/红包.png',
+        title:'红包套餐'
+      },
+      {
+        imgUrl:'http://www.dell-lee.com/imgs/vue3/超市.png',
+        title:'超市便利'
+      },
+      {
+        imgUrl:'http://www.dell-lee.com/imgs/vue3/超市.png',
+        title:'超市便利'
+      },
+      {
+        imgUrl:'http://www.dell-lee.com/imgs/vue3/超市.png',
+        title:'超市便利'
+      },
+      {
+        imgUrl:'http://www.dell-lee.com/imgs/vue3/超市.png',
+        title:'超市便利'
+      },
+      {
+        imgUrl:'http://www.dell-lee.com/imgs/vue3/超市.png',
+        title:'超市便利'
+      },
+      {
+        imgUrl:'http://www.dell-lee.com/imgs/vue3/超市.png',
+        title:'超市便利'
+      },
+      {
+        imgUrl:'http://www.dell-lee.com/imgs/vue3/超市.png',
+        title:'超市便利'
+      },
+      {
+        imgUrl:'http://www.dell-lee.com/imgs/vue3/超市.png',
+        title:'超市便利'
+      },
+      {
+        imgUrl:'http://www.dell-lee.com/imgs/vue3/超市.png',
+        title:'超市便利'
+      },
+    ]
+    return { iconsList }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../style/viriables.scss';
+@import '../../style/mixins.scss';
+.position{
+  position: relative;
+  padding: .16rem .24rem .16rem 0;
+  line-height: .22rem;
+  font-size: .16rem;
+  @include ellipse;
+  .position__icon{
+    position: relative;
+    top: .01rem;
+    font-size: .2rem;
+  }
+  .position__notice{
+    position: absolute;
+    right: 0;
+    top: .17rem;
+    font-size: .2rem;
+  }
+  color: $content-fontcolor;
+}
+.search{
+  margin-bottom: .12rem;
+  height: .32rem;
+  line-height: .32rem;
+  background: #F5F5F5;
+  color: #B7B7B7;
+  border-radius: .16rem;
+  .iconfont{
+    display: inline-block;
+    padding: 0 .08rem 0 .16rem;
+    font-size: .16rem;
+  }
+  &__text{
+    display: inline-block;
+    font-size: .14rem;
+  }
+}
+.banner{
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 25.4%;
+  &__img{
+    width: 100%;
+  }
+}
+.icons{
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: .16rem;
+  &__item{
+    width: 20%;
+    &__img{
+      display: block;
+      width: .4rem;
+      height: .4rem;
+      margin: 0 auto;
+    }
+    &__desc{
+      margin:.06rem 0 .16rem 0;
+      text-align: center;
+      color: $content-fontcolor;
+    }
+  }
+}
+.gap{
+  margin: 0 -.18rem;
+  height: .1rem;
+  background: $content-bgColor;
+  
+}
+</style>
